d13e291a96c5939f1c64c9f5d79417947705985d
ivan-burgio <ivan.24.burgio@gmail.com>
2024-02-21 13:31:32 -0300

    Reestructuracion de categorias

--- src/js/barras.js
+++ src/js/barras.js
@@ -1,11 +1,13 @@
 const barras = document.querySelectorAll('.barras');
 const categorias = document.querySelector('#categorias');
 const screen = document.querySelector('.categorias__screen');
+const body = document.querySelector('body');
 
 // Agrega evento de clic a los elementos con la clase 'barras'
 barras.forEach(barra => {
     barra.addEventListener('click', () => {
         screen.classList.add('d-flex');
+        body.classList.add('overflow-hidden');
     });
 });
 
@@ -14,5 +16,6 @@ document.addEventListener('click', (event) => {
     // Verifica si el clic no ocurrió dentro del contenedor
     if (!categorias.contains(event.target) && !event.target.classList.contains('barras')) {
         screen.classList.remove('d-flex');
+        body.classList.remove('overflow-hidden');
     }
 });
--- views/templates/categorias.php
+++ views/templates/categorias.php
@@ -1,19 +1,37 @@
 <div class="categorias__screen">
     <div id="categorias" class="categorias">
-        <div class="categorias__secciones">
-            <a href="/" class="categorias__link">Novedades</a>
-            <a href="/noticias" class="categorias__link">Noticias</a>
-            <a href="/analisis" class="categorias__link">Analisis</a>
-            <a href="/articulos" class="categorias__link">Articulos</a>
-            <a href="/guias" class="categorias__link">Guias</a>
-        </div>
-    
-        <div class="categorias__secciones">
-            <a href="/" class="categorias__link">Novedades</a>
-        </div>
-    
-        <div class="categorias__secciones">
-            <a href="/" class="categorias__link">Novedades</a>
+        <h3>Categorias</h3>
+
+        <div class="categorias__seccion">
+            <div class="categorias__seccion--columna border-end border-1 border-black">
+                <a href="/" class="categorias__seccion--link">Novedades</a>
+                <a href="/noticias" class="categorias__seccion--link">Noticias</a>
+                <a href="/analisis" class="categorias__seccion--link">Analisis</a>
+                <a href="/articulos" class="categorias__seccion--link">Articulos</a>
+                <a href="/guias" class="categorias__seccion--link">Guias</a>
+            </div>
+
+            <div class="categorias__seccion">
+                <div class="categorias__seccion--columna">
+                    <a href="/" class="categorias__seccion--link">Videojuegos</a>
+                    <a href="/" class="categorias__seccion--link">Plataformas</a>
+                    <a href="/" class="categorias__seccion--link">Tecnologia</a>
+                    <a href="/" class="categorias__seccion--link">Programación</a>
+                    <a href="/" class="categorias__seccion--link">Anime</a>
+                    <a href="/" class="categorias__seccion--link">Series</a>
+                </div>
+
+                <div class="categorias__seccion--columna">
+                    <a href="/" class="categorias__seccion--link">Novedades</a>
+                    <a href="/" class="categorias__seccion--link">Novedades</a>
+                    <a href="/" class="categorias__seccion--link">Novedades</a>
+                    <a href="/" class="categorias__seccion--link">Novedades</a>
+                    <a href="/" class="categorias__seccion--link">Novedades</a>
+                    <a href="/" class="categorias__seccion--link">Novedades</a>
+                    <a href="/" class="categorias__seccion--link">Novedades</a>
+                    <a href="/" class="categorias__seccion--link">Novedades</a>
+                </div>
+            </div>
         </div>
     </div>
 </div>
