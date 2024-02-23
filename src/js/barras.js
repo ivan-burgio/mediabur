394acda4e2a62ace396daf59a2c6a59eb18c34ec
ivan-burgio <ivan.24.burgio@gmail.com>
2024-02-23 10:57:39 -0300

    Reestructuracion del codigo de JS de categorias a JQuery

--- src/js/barras.js
+++ src/js/barras.js
@@ -1,21 +1,21 @@
-const barras = document.querySelectorAll('.barras');
-const categorias = document.querySelector('#categorias');
-const screen = document.querySelector('.categorias__screen');
-const body = document.querySelector('body');
+$(document).ready(function() {
+    var barras = $('.barras');
+    var categorias = $('#categorias');
+    var screen = $('.categorias__screen');
+    var body = $('body');
 
-// Agrega evento de clic a los elementos con la clase 'barras'
-barras.forEach(barra => {
-    barra.addEventListener('click', () => {
-        screen.classList.add('d-flex');
-        body.classList.add('overflow-hidden');
+    // Agrega evento de clic a los elementos con la clase 'barras'
+    barras.on('click', function() {
+        screen.addClass('d-flex');
+        body.addClass('overflow-hidden');
     });
-});
 
-// Agrega evento de clic al documento para cerrar el contenedor si se hace clic fuera de él
-document.addEventListener('click', (event) => {
-    // Verifica si el clic no ocurrió dentro del contenedor
-    if ((!categorias.contains(event.target) && !event.target.classList.contains('barras')) || event.target.classList.contains('categorias__titulo--icono')) {
-        screen.classList.remove('d-flex');
-        body.classList.remove('overflow-hidden');
-    }
+    // Agrega evento de clic al documento para cerrar el contenedor si se hace clic fuera de él
+    $(document).on('click', function(event) {
+        // Verifica si el clic no ocurrió dentro del contenedor
+        if ((!categorias.is(event.target) && !$(event.target).hasClass('barras')) || $(event.target).hasClass('categorias__titulo--icono')) {
+            screen.removeClass('d-flex');
+            body.removeClass('overflow-hidden');
+        }
+    });
 });
