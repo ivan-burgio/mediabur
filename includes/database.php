55e61a12c758ead7a1d4c28c1bbaf96b64d70ca6
ivan-burgio <ivan.24.burgio@gmail.com>
2024-02-27 11:00:36 -0300

    Conexion a la db

--- includes/database.php
+++ includes/database.php
@@ -1,6 +1,6 @@
 <?php
 
-/*$db = mysqli_connect(
+$db = mysqli_connect(
     $_ENV['DB_HOST'] ?? '',
     $_ENV['DB_USER'] ?? '',
     $_ENV['DB_PASS'] ?? '',
@@ -15,4 +15,3 @@ if (!$db) {
     echo "error de depuración: " . mysqli_connect_error();
     exit;
 }
-*/
