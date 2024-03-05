74f1cec160cdbbe7f7fa7a1bfa2bc958b0e26bbf
ivan-burgio <ivan.24.burgio@gmail.com>
2024-03-05 11:14:34 -0300

    Correcta configuracion del env y db en la laptop

--- mediabur.sql
+++ mediabur.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 01-03-2024 a las 14:27:49
+-- Tiempo de generación: 05-03-2024 a las 15:10:10
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
