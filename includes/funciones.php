9af7ad36fa68189cec51d23e82fbb6785f435c6f
ivan-burgio <ivan.24.burgio@gmail.com>
2024-03-05 13:42:55 -0300

    Cambio en funcion del globo a template y mejora en el mismo

--- includes/funciones.php
+++ includes/funciones.php
@@ -22,20 +22,7 @@ function isAuth(): void {
 
 function isAdmin() {
     if (isset($_SESSION['login'])) {
-        echo '
-            <div class="globo">
-                <i class="globo__icono fa-solid fa-sliders""></i>
-            </div>
-
-            <div class="globo__pincho">
-                <a class="globo__pincho-x"><i class="fa-solid fa-x fa-lg globo__pincho-x--icono"></i></a>
-                <a class="globo__pincho-link" href="/dashboard">Dashboard</a>
-                <a class="globo__pincho-link">Crear Publicación</a>
-                <a class="globo__pincho-link">Editar Publicación</a>
-                <a class="globo__pincho-link">Eliminar Publicación</a>
-                <a class="globo__pincho-link" href="/logout">Logout</a>
-            </div>
-        ';
+        include __DIR__ . '/../views/admin-templates/globo.php';
     }
 }
 
---
+++ views/admin-templates/globo.php
@@ -0,0 +1,40 @@
+<div class="globo">
+    <i class="globo__icono fa-solid fa-sliders""></i>
+            </div>
+
+            <div class=" globo__pincho">
+        <a class="globo__pincho-x"><i class="fa-solid fa-x fa-lg globo__pincho-x--icono"></i></a>
+        <a class="globo__pincho-link" href="/dashboard">Dashboard</a>
+
+        <p class="dropdown-toggle globo__pincho-link" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Crear Publicación
+        </p>
+        <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Noticia</a></li>
+            <li><a class="dropdown-item" href="#">Guia</a></li>
+            <li><a class="dropdown-item" href="#">Articulo</a></li>
+            <li><a class="dropdown-item" href="#">Analisis</a></li>
+        </ul>
+
+        <p class="dropdown-toggle globo__pincho-link" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Editar Publicación
+        </p>
+        <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Noticia</a></li>
+            <li><a class="dropdown-item" href="#">Guia</a></li>
+            <li><a class="dropdown-item" href="#">Articulo</a></li>
+            <li><a class="dropdown-item" href="#">Analisis</a></li>
+        </ul>
+
+        <p class="dropdown-toggle globo__pincho-link" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Eliminar Publicación
+        </p>
+        <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Noticia</a></li>
+            <li><a class="dropdown-item" href="#">Guia</a></li>
+            <li><a class="dropdown-item" href="#">Articulo</a></li>
+            <li><a class="dropdown-item" href="#">Analisis</a></li>
+        </ul>
+
+        <a class="globo__pincho-link" href="/logout">Logout</a>
+</div>
