28408fc363cc59bb7bf620b2d5d6bb700d8e5c05
ivan-burgio <ivan.24.burgio@gmail.com>
2024-03-19 12:59:05 -0300

    Condicional de aviso por si no hay publicaciones de ese tipo

--- views/pages/general.php
+++ views/pages/general.php
@@ -3,12 +3,16 @@
         <h1><?php echo $titulo; ?></h1>
 
         <div class="tarjetas">
+            <?php if (empty($publicaciones)) { ?>
+                <p>No hay publicaciónes de este tipo creadas.</p>
+            <?php } ?>
+
             <?php
-                foreach ($publicaciones as $publi) {
-                    if($publi->activo == '1') {
-                        echo tarjetaClasicaTemplate($publi->portada, $publi->titulo, $publi->categoria, $publi->fecha);
-                    }
+            foreach ($publicaciones as $publi) {
+                if ($publi->activo == '1') {
+                    echo tarjetaClasicaTemplate($publi->portada, $publi->titulo, $publi->categoria, $publi->fecha);
                 }
+            }
             ?>
         </div>
     </div>
