89c73a0d551c1df8c9e0f4a31bc1d1f3bd59c929
Iván Burgio <117323421+ivan-burgio@users.noreply.github.com>
2024-03-26 17:50:12 -0300

    Update publicacion.php

--- views/pages/publicacion.php
+++ views/pages/publicacion.php
@@ -1,11 +1,5 @@
 <div class="main_contenido">
     <div class="main_principal">
-<<<<<<< HEAD
-<<<<<<< HEAD
-        <h1><?php echo $titulo; ?></h1>
-=======
-=======
->>>>>>> 55c9968f584d441d88a2ab6be40ade82b7fc7be7
         <?php if ($publi->activo == 0) { ?>
             <p class="publicacion__no-activa">PUBLICACIÓN NO ACTIVA</p>
         <?php } ?>
@@ -13,18 +7,10 @@
 
         <div class="publicacion">
             <h1><?php echo $titulo; ?></h1>
-<<<<<<< HEAD
             <img src="<?php echo $publi->portada ?>" alt="<?php echo $publi->titulo; ?>" loading="lazy">
             <?php echo $publi->texto; ?>
         </div>
 
->>>>>>> ae7b193 (Mejoras visuales y Responsive)
-=======
-            <img src="<?php echo $publi->portada ?>" alt="<?php echo $publi->titulo; ?>">
-            <?php echo $publi->texto; ?>
-        </div>
-
->>>>>>> 55c9968f584d441d88a2ab6be40ade82b7fc7be7
     </div>
 
     <div class="ad d-none d-md-block">Publicidad</div>
